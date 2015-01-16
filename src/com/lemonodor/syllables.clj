09646a1769016f0cb35ba6188fd50aa44c79f47b
wiseman <jjwiseman@gmail.com>
2015-01-15 17:30:33 -0800

    Added db based on cmudict 0.7b.

--- src/com/lemonodor/syllables.clj
+++ src/com/lemonodor/syllables.clj
@@ -1,6 +1,56 @@
-(ns clj-syllables.core)
+(ns com.lemonodor.syllables
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.set :as set]
+            [clojure.string :as string])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn parse-cmudict-line [line]
+  (string/split (string/lower-case line) #"(\(\d+\))? +"))
+
+
+(def digit-chars #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9})
+
+
+(defn count-cmudict-syllables [phonemes]
+  (count
+   (filter
+    (fn [^String p]
+      (digit-chars (.charAt p (dec (.length p)))))
+    phonemes)))
+
+
+(defn make-syllable-db-from-cmudict [path]
+  (with-open [rdr (io/reader path)]
+    (apply
+     merge-with
+     set/union
+     (map (fn [[word & phonemes]]
+            {word #{(count-cmudict-syllables phonemes)}})
+          (map parse-cmudict-line
+               (filter
+                (fn [^String l] (not (= (.charAt l 0) \;)))
+                (line-seq rdr)))))))
+
+
+(def default-syllables-db (atom nil))
+
+
+(defn count-syllables
+  ([word]
+   (when-not @default-syllables-db
+     (swap! default-syllables-db
+            (fn [_]
+              (edn/read-string
+               (slurp (io/resource "com/lemonodor/syllables/syllables.db"))))))
+   (count-syllables @default-syllables-db word))
+  ([sdb word]
+   (sdb (string/lower-case word))))
+
+
+(defn -main [& args]
+  (let [sdb (make-syllable-db-from-cmudict (first args))]
+    (binding [*out* *err*]
+      (println "Creating syllable database with" (count sdb) "words."))
+    (prn sdb)))
--- test/com/lemonodor/syllables_test.clj
+++ test/com/lemonodor/syllables_test.clj
@@ -1,7 +1,33 @@
-(ns clj-syllables.core-test
+(ns com.lemonodor.syllables-test
   (:require [clojure.test :refer :all]
-            [clj-syllables.core :refer :all]))
+            [com.lemonodor.syllables :as syllables]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest cmudict-test
+  (testing "parse-cmudict-line"
+    (is (= ["\"unquote" "ah1" "n" "k" "w" "ow1" "t"]
+           (syllables/parse-cmudict-line "\"UNQUOTE  AH1 N K W OW1 T")))
+    (is (= ["aaa" "t" "r" "ih2" "p" "ah0" "l" "ey1"]
+           (syllables/parse-cmudict-line "AAA  T R IH2 P AH0 L EY1")))
+    (is (= ["aaronson's" "aa1" "r" "ah0" "n" "s" "ah0" "n" "z"]
+           (syllables/parse-cmudict-line
+            "AARONSON'S(1)  AA1 R AH0 N S AH0 N Z")))
+    (is (= ["abkhazian" "ae0" "b" "k" "ae1" "z" "y" "ah0" "n"]
+           (syllables/parse-cmudict-line
+            "ABKHAZIAN(3)  AE0 B K AE1 Z Y AH0 N"))))
+  (testing "count-cmudict-syllables"
+    (is (= 3
+           (syllables/count-cmudict-syllables
+            ["m" "ah0" "k" "aa1" "r" "th" "iy0"])))
+    (is (= 1 (syllables/count-cmudict-syllables ["l" "ih1" "s" "p"]))))
+  (testing "make-syllable-db-from-cmudict"
+    (let [cmudict (str ";; Test dict\n"
+                       "LLAMAS  L AA1 M AH0 Z\n"
+                       "LOUIS'(1)  L UW1 IY0 Z\n"
+                       "LOUIS'(2)  L UW1 IH0 S IH0 Z\n")
+          sdb (syllables/make-syllable-db-from-cmudict (.getBytes cmudict))]
+      (is (= {"louis'" #{3 2}, "llamas" #{2}} sdb))
+      (is (= #{2 3} (syllables/count-syllables sdb "louis'")))
+      (is (= #{2 3} (syllables/count-syllables sdb "LouIS'")))))
+  (testing "count-syllables"
+    (is (= #{4} (syllables/count-syllables "ridiculous")))
+    (is (= nil (syllables/count-syllables "........")))))
